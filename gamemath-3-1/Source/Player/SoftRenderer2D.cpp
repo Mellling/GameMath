0e0516d986a5bee40f86e99fe15bb4b82c5c1e7b
Mellling <bluebell1183@gmail.com>
2025-08-17 19:07:21 +0900

    feat: 3-1 예제 기반 실습 구현 완료

--- gamemath-3-1/Source/Player/SoftRenderer2D.cpp
+++ gamemath-3-1/Source/Player/SoftRenderer2D.cpp
@@ -2,6 +2,7 @@
 #include "Precompiled.h"
 #include "SoftRenderer.h"
 #include <random>
+#include <sstream>
 using namespace CK::DD;
 
 // 격자를 그리는 함수
@@ -52,32 +53,69 @@ void SoftRenderer::LoadScene2D()
 
 }
 
-// 게임 로직과 렌더링 로직이 공유하는 변수
+// 게임 로직(Update2D)과 렌더링 로직(Render2D)이 공유하는 변수
+Vector2 currentPosition(0.f, 0.f);	// 기본값을 (0, 0)으로 지정
 
 
-// 게임 로직을 담당하는 함수
+// 게임 로직을 담당하는 함수 (사용자의 입력에 따른 처리를 다룸)
 void SoftRenderer::Update2D(float InDeltaSeconds)
 {
 	// 게임 로직에서 사용하는 모듈 내 주요 레퍼런스
-	auto& g = Get2DGameEngine();
+	auto& g = Get2DGameEngine();	// 개암 앤잔 모듈애 접근
+	// 사용자 입력값을 읽어들일 수 있도록 
+	// 입력 관리자 InputManager의 레퍼런스를 읽기 전용으로 가져옴
 	const InputManager& input = g.GetInputManager();
 
 	// 게임 로직의 로컬 변수
+	static float moveSpeed = 100.f;		// 프레임당 이동 속도를 지정
 
+	// X축과 Y축 입력을 결합해 입력 벡터를 지정
+	Vector2 inputValue = Vector2
+	(
+		input.GetAxis(InputAxis::XAxis), 
+		input.GetAxis(InputAxis::YAxis)
+	);
+
+	// 이동 속도, 프레임의 경과 시간(InDeltaSeconds)을 곱하여 해당 프레임이 이동할 길이를 계산
+	// 이를 입력 벡터에 곱하고 그 결과를 deltaPosition애 저장
+	Vector2 deltaPosition = inputValue * moveSpeed * InDeltaSeconds;
+
+	// 물체의 최종 상태 결정
+	currentPosition += deltaPosition;	// deltaPosition 값을 반영하여 현재 프레임의 currentPosition 값을 확정
 }
 
 // 렌더링 로직을 담당하는 함수
+// 게임 로직에서 계산한 데이터를 반영해 최종 결과를 모니터 화면에 보여주는 기능을 구현함
 void SoftRenderer::Render2D()
 {
 	// 렌더링 로직에서 사용하는 모듈 내 주요 레퍼런스
-	auto& r = GetRenderer();
-	const auto& g = Get2DGameEngine();
+	auto& r = GetRenderer();	// 화면의 시각적 표현을 위해 렌더러 모듈의 레퍼런스를 가져옴
+	const auto& g = Get2DGameEngine();	// 게임 엔진 모듈의 레퍼런스를 읽기 전용으로 가져옴
 
 	// 배경에 격자 그리기
 	DrawGizmo2D();
 
 	// 렌더링 로직의 로컬 변수
 
+	// 마젠타 색의 선을 사용해 평행한 백터를 표현
+	static float lineLength = 500.f;
+	Vector2 lineStart = currentPosition * lineLength;
+	Vector2 lineEnd = currentPosition * -lineLength;
+	r.DrawLine(lineStart, lineEnd, LinearColor::Magenta);
+
+	// 백터를 파란색 픽셀(8 픽셀)로 표현
+	r.DrawPoint(currentPosition, LinearColor::Blue);
+	r.DrawPoint(currentPosition + Vector2::UnitX, LinearColor::Blue);
+	r.DrawPoint(currentPosition + Vector2::UnitY, LinearColor::Blue);
+	r.DrawPoint(currentPosition - Vector2::UnitX, LinearColor::Blue);
+	r.DrawPoint(currentPosition - Vector2::UnitY, LinearColor::Blue);
+	r.DrawPoint(currentPosition + Vector2::One, LinearColor::Blue);
+	r.DrawPoint(currentPosition - Vector2::One, LinearColor::Blue);
+	r.DrawPoint(currentPosition + Vector2(1.f, -1.f), LinearColor::Blue);
+	r.DrawPoint(currentPosition - Vector2(1.f, -1.f), LinearColor::Blue);
+
+	// 백터의 현재 좌표를 우상단에 표시
+	r.PushStatisticText("Coordination : " + currentPosition.ToString());
 }
 
 // 메시를 그리는 함수
